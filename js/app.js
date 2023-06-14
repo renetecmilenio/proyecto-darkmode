69979347bbbacc521d6329f6178ad88cdea109fd
Rene Cerón <rene.ceron@tec.mx>
2023-06-14 11:24:03 -0600

    sE AGREGARÓN MÁS COMENTARIOS

--- js/app.js
+++ js/app.js
@@ -1,36 +1,44 @@
 // DARK MODE
 
-// SE DECLARA LA CONSTANTE QUE ES EL ELEMENTO DEL DOM (dOCUMENT OBJECT MODEL) 
+// SE DECLARA LA VARIABLE CONSTANTE QUE ES EL ELEMENTO "BOTON" DEL DOM (DOCUMENT OBJECT MODEL)
 const btnSwitch = document.querySelector("#switch");
 
-// FUNCIÓN DE EVENTO "CLICK"
+// FUNCIÓN ESCUCHA DE EVENTO "CLICK" APLICADO AL BOTON MEDIANTE LA VARIABLE CONTANTE QUE SE GUARDÓ
 btnSwitch.addEventListener("click", () => {
-  
-  // AL DAR CLICK SE AGREGA O SE QUITA LA CLASE "DARK" AL BODY
-  document.body.classList.toggle("dark");
-  // AL DAR CLICK SE AGREGA O SE QUITA LA CLASE "ACTIVE" AL BOTÓN
-  btnSwitch.classList.toggle("active");
+  /*
+  document = el documento en si osea la pagina actual
+  body = es una "propiedad" del documento con la cual accedes al cuerpo del documento
+  classList = "propiedad" que enlista las clases que se establecieron en el objeto que se establezca
+  toogle = "método" que agrega o remueve el "token" que se le pase como parámetro  
+  */
+  document.body.classList.toggle("dark"); // AL DAR CLICK AL "BOTON" SE AGREGA O SE QUITA LA CLASE "DARK" AL BODY
 
-  // guardar el modo dark en local storage con una variable y un valor
+  btnSwitch.classList.toggle("active"); // AL DAR CLICK "BOTON" SE AGREGA O SE QUITA LA CLASE "ACTIVE" AL BOTÓN
+
+  // ----------------
+
+  // condicional que guardar el modo dark en local storage mediante una variable (dark-mode) y un valor (true/false) esto al dar click en el boton
+
+  /*
+contains() = "Método" devuelve verdadero si la lista contiene el token dado; de lo contrario, falso.
+localStorage = "Propiedad"  te permite acceder al objeto local Storage; los datos persisten almacenados en navegador
+setItem = "Método" cuando reciba una clave y un valor, añadirá estos al almacén, o actualizará el valor si la clave ya existe.
+*/
   if (document.body.classList.contains("dark")) {
-    localStorage.setItem("dark-mode", "true");
+    localStorage.setItem("dark-mode", "true"); //
   } else {
     localStorage.setItem("dark-mode", "false");
   }
 
 });
 
-
 /*
-al empezar o actualizar la página pregunta si esta establecida la variable "dark-mode" con el valor de "true" 
-en local storage significa que esta previamente seleccionado el modo dark, si esta establecida agrega la clase dark al body y la clase active al botón y si no (else) se la remueven ambas clases a los elementos
-*/ 
+  COMO NO ESTA DENTRO DE UN EVENTO Al empezar o actualizar la página esta "CONDICIONAL"  valida si esta establecida la variable "dark-mode" con el valor de "true" en local storage, si la condicion da verdaderA significa que previamente se seleccionó el modo dark y agrega la clase "dark" al body y la clase "active" al botón y si no (else) se la remueven ambas clases a los elementos
+  */
 if (localStorage.getItem("dark-mode") === "true") {
-  document.body.classList.add("dark");
-  btnSwitch.classList.add("active");
+  document.body.classList.add("dark"); // A DIFERENCIA DEL MÉTODO "TOOGLE" SOLO SE "AGREGA" (NO INTERCAMBIA) LA CLASE "DARK" AL BODY
+  btnSwitch.classList.add("active"); // A DIFERENCIA DEL MÉTODO "TOOGLE" SOLO SE "AGREGA" (NO INTERCAMBIA) LA CLASE "ACTIVE" AL BOTÓN
 } else {
-  document.body.classList.remove("dark");
-  btnSwitch.classList.remove("active");
+  document.body.classList.remove("dark");// A DIFERENCIA DEL MÉTODO "TOOGLE" SOLO SE "REMUEVE" (NO INTERCAMBIA) LA CLASE "DARK" AL BODY
+  btnSwitch.classList.remove("active"); // A DIFERENCIA DEL MÉTODO "TOOGLE" SOLO SE "REMUEVE" (NO INTERCAMBIA) LA CLASE "ACTIVE" AL BOTÓN
 }
-
-
